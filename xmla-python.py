eff3946164a97cc1294f8a7671ad57190d136b37
ARSHAD AZEEM <arshadazeem@gmail.com>
2021-04-28 10:41:57 -0400

    Add files via upload

---
+++ xmla-python.py
@@ -0,0 +1,23 @@
+# Databricks notebook source
+import requests
+import pandas as pd
+from requests.auth import HTTPBasicAuth
+
+url = 'https://<myappsvc>.azurewebsites.net/api/Query?query=Evaluate <Database_Name>'
+user = 'app:<clientId><@TenantId>' # e.g. 'app:123456-7890-1234-5678-901234@123456-7890-1234-5678-9012345'
+
+secret = 'mysecret'
+
+# call REST endpoint (Databricks --> App Service REST API --> Azure Analysis Service)
+res = requests.get(url, auth=HTTPBasicAuth(user, secret))
+
+# load json in dataframe
+data = pd.read_json(res.content)
+
+#print
+display(data)
+
+
+# COMMAND ----------
+
+
