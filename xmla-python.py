4af151160e4d0157a43c07b3b9577ca6dec40565
ARSHAD AZEEM <arshadazeem@gmail.com>
2021-04-28 10:43:38 -0400

    Update xmla-python.py

--- xmla-python.py
+++ xmla-python.py
@@ -3,9 +3,11 @@ import requests
 import pandas as pd
 from requests.auth import HTTPBasicAuth
 
+# Rest Endpoint to invoke, along with the azure analysis services query as query string
 url = 'https://<myappsvc>.azurewebsites.net/api/Query?query=Evaluate <Database_Name>'
-user = 'app:<clientId><@TenantId>' # e.g. 'app:123456-7890-1234-5678-901234@123456-7890-1234-5678-9012345'
 
+#service principal credentials
+user = 'app:<clientId><@TenantId>' # e.g. 'app:123456-7890-1234-5678-901234@123456-7890-1234-5678-9012345'
 secret = 'mysecret'
 
 # call REST endpoint (Databricks --> App Service REST API --> Azure Analysis Service)
